825c65216f617def6b33779551440d5124c36e36
Praveen K <118113479+praveen9655@users.noreply.github.com>
2023-08-26 05:32:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -26,21 +26,57 @@ function updateCountdown() {
     var opn= document.querySelector('.Regi');
     opn.style.display='block';
   }
-//Sheet 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwVAB5kCoEUqTXCu6lsOazM0A0-Z_ugMpfwjIKeT3pWaI0XtFhnZYqqZCtlvK3XaegNqw/exec'
-  const form = document.forms['form']
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log("Thank you! your form is submitted successfully." ))
-      // .then(() => {  window.location.reload(); })
-      .then(() => {  
-        document.querySelector("#form").style.display='none';
-        document.querySelector("#success").style.display='block';
-       })
-      .catch(error => console.error('Error!', error.message))
-  })
+//Sheet 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxiGtyhTf8A1jIsKcR3GGbpzigrQ59NDbUqSk4PCCicrTLL8ykYDRUOvYBZZ5Zvud_hlQ/exec'
+
+const form = document.forms['form']
+//   form.addEventListener('submit', e => {
+//     e.preventDefault();
+//     document.querySelector("#loading").style.display = 'block';
+//     document.querySelector("#form").style.display = 'none';
+//     document.querySelector("#success").style.display = 'none';
+
+//     function getCurrentDateTime() {
+//         const now = new Date();
+        
+//       const day = String(now.getDate()).padStart(2, '0');
+//       const month = String(now.getMonth() + 1).padStart(2, '0');
+//       const year = now.getFullYear();
+      
+//       const hours = String(now.getHours()).padStart(2, '0');
+//       const minutes = String(now.getMinutes()).padStart(2, '0');
+      
+//       const formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}`;
+//         return formattedDateTime;
+//     }
+
+//     const currentDateTime = getCurrentDateTime();
+//     const formData = new FormData(form);
+//     formData.append('Time', currentDateTime);
+
+//     fetch(scriptURL, { method: 'POST', body: formData })
+//     .then(response => response.json())
+//     .then(data => {
+//         if (data.result === 'success') {
+//             console.log("Thank you! Your form is submitted successfully.");
+//             document.querySelector("#form").style.display = 'none';
+//             document.querySelector("#loading").style.display = 'none';
+//             document.querySelector("#success").style.display = 'block';
+//         } else {
+//             console.error('Error!', data.error);
+//             document.querySelector("#errorMessage").textContent = data.error; // Show the error message
+//         }
+//     })
+//     .catch(error => console.error('Error!', error.message));
+
+// });
+
+
+// alert('duplicate value');
+// document.querySelector("#form").style.display = 'block';
+// document.querySelector("#loading").style.display = 'none';
+// document.querySelector("#success").style.display = 'none';
 //FireBase
 
   const firebaseConfig = {
@@ -56,39 +92,85 @@ firebase.initializeApp(firebaseConfig);
 var contactFormDB = firebase.database().ref('FormTest');
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const form = document.querySelector('#form');
-    var studentName = document.getElementById("studentName").value;
-    var studentEmail = document.getElementById("studentEmail").value;
-    var studentPhone = document.getElementById("studentPhone").value;
-    var collegeName = document.getElementById("collegeName").value;
-    var department = document.querySelector('input[name="department"]:checked').value;
-    var yearOfStudying = document.querySelector('input[name="yearOfStudying"]:checked').value;
-    
-        var technicalEvents = [];
-        var technicalEventCheckboxes = document.querySelectorAll('input[name="technicalEvents[]"]:checked');
-        technicalEventCheckboxes.forEach(function(checkbox) {
-            technicalEvents.push(checkbox.value);
-        });
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-        var nonTechnicalEvents = [];
-        var nonTechnicalEventCheckboxes = document.querySelectorAll('input[name="nonTechnicalEvents[]"]:checked');
-        nonTechnicalEventCheckboxes.forEach(function(checkbox) {
-            nonTechnicalEvents.push(checkbox.value);
-        });
-    console.log(studentName,studentEmail,studentPhone,collegeName, department ,yearOfStudying, technicalEvents, nonTechnicalEvents);
-  var newContactForm = contactFormDB.push();
-    newContactForm.set({
-            studentName: studentName,
-            studentEmail: studentEmail,
-            studentPhone: studentPhone,
-            collegeName: collegeName,
-            department: department,
-            yearOfStudying: yearOfStudying,
-            technicalEvents: technicalEvents,
-            nonTechnicalEvents: nonTechnicalEvents
-        });
+  const studentPhone = document.getElementById("studentPhone").value;
+  const duplicateCheck = await checkDuplicateStudentPhone(studentPhone);
+
+  if (duplicateCheck) {
+      alert("Duplicate student phone number. Submission not allowed.");
+  } else {
+      const studentName = document.getElementById("studentName").value;
+      const studentEmail = document.getElementById("studentEmail").value;
+      const collegeName = document.getElementById("collegeName").value;
+      const department = document.querySelector('input[name="department"]:checked').value;
+      const yearOfStudying = document.querySelector('input[name="yearOfStudying"]:checked').value;
+      
+      const technicalEvents = [];
+      const technicalEventCheckboxes = document.querySelectorAll('input[name="technicalEvents[]"]:checked');
+      technicalEventCheckboxes.forEach(function(checkbox) {
+          technicalEvents.push(checkbox.value);
+      });
+
+      const nonTechnicalEvents = [];
+      const nonTechnicalEventCheckboxes = document.querySelectorAll('input[name="nonTechnicalEvents[]"]:checked');
+      nonTechnicalEventCheckboxes.forEach(function(checkbox) {
+          nonTechnicalEvents.push(checkbox.value);
+      });
+
+      const newContactForm = contactFormDB.push();
+      newContactForm.set({
+          studentName: studentName,
+          studentEmail: studentEmail,
+          studentPhone: studentPhone,
+          collegeName: collegeName,
+          department: department,
+          yearOfStudying: yearOfStudying,
+          technicalEvents: technicalEvents,
+          nonTechnicalEvents: nonTechnicalEvents
+      });
+
+      document.querySelector("#loading").style.display = 'block';
+      document.querySelector("#form").style.display = 'none';
+      document.querySelector("#success").style.display = 'none';
+  
+      function getCurrentDateTime() {
+          const now = new Date();
+          
+        const day = String(now.getDate()).padStart(2, '0');
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const year = now.getFullYear();
+        
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        
+        const formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}`;
+          return formattedDateTime;
+      }
+  
+      const currentDateTime = getCurrentDateTime();
+      const formData = new FormData(form);
+      formData.append('Time', currentDateTime);
+  
+      fetch(scriptURL, { method: 'POST', body: formData })
+      .then(response => response.json())
+      .then(data => {
+          if (data.result === 'success') {
+              console.log("Thank you! Your form is submitted successfully.");
+              document.querySelector("#form").style.display = 'none';
+              document.querySelector("#loading").style.display = 'none';
+              document.querySelector("#success").style.display = 'block';
+          } else {
+              console.error('Error!', data.error);
+              document.querySelector("#errorMessage").textContent = data.error; // Show the error message
+          }
+      })
+      .catch(error => console.error('Error!', error.message));
+  }
 });
-    var el = document.getElementById("wrapper");
-    var toggleButton = document.getElementById("menu-toggle");
+
+async function checkDuplicateStudentPhone(phoneNumber) {
+  const snapshot = await contactFormDB.orderByChild("studentPhone").equalTo(phoneNumber).once("value");
+  return snapshot.exists();
+}
